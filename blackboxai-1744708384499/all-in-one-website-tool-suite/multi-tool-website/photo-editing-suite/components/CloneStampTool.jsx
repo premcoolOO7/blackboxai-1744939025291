6958acb5f048933039d3f39ed41839a99dc774ce
premcoolOO7 <gauravprem75666@gmail.com>
2025-04-18 06:53:14 +0530

    Add blackboxai-1744708384499/all-in-one-website-tool-suite/multi-tool-website/photo-editing-suite/components/CloneStampTool.jsx 1744939393687

---
+++ blackboxai-1744708384499/all-in-one-website-tool-suite/multi-tool-website/photo-editing-suite/components/CloneStampTool.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function CloneStampTool() {
+  return (
+    <div className="text-gray-300">
+      <p>
+        Clone Stamp Tool: Remove blemishes and unwanted objects by cloning pixels from one area to another.
+      </p>
+      <p className="mt-2 italic text-sm text-purple-400">
+        (This is a placeholder UI. Integration with Canvas-based clone stamp brush to be implemented.)
+      </p>
+    </div>
+  );
+}
